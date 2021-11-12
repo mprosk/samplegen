1f22960c0413ca0a97ff6ca14abdba67e324e967
Max Proskauer <mprosk@gmail.com>
2021-11-12 00:18:09 -0500

    Get basic functionality for sine waves

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use std::f64::consts::PI;
+use std::process::exit;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "samplegen")]
+/// Generate samples from periodic functions
+struct Config {
+    /// Bit depth
+    #[structopt(short, long)]
+    depth: u8,
+
+    /// Number of samples to generate
+    #[structopt(short, long)]
+    samples: u32,
+
+    /// Enables hexadecimal output
+    #[structopt(short, long)]
+    hex: bool,
+
+    /// Verbose mode
+    #[structopt(short, long)]
+    verbose: bool,
+}
+
+fn main() {
+    let config = Config::from_args();
+    println!("{:#?}", config);
+
+    // Check the bounds of the bit depth
+    if config.depth == 0 {
+        println!("Specified bit depth cannot be zero");
+        exit(1);
+    }
+    if config.depth > 32 {
+        println!(
+            "Specified bit depth of {} exceeds supported maximum of 32",
+            config.depth
+        );
+        exit(2);
+    }
+
+    // Calculate half of the maximum sample value
+    let max = (2_u64.pow(config.depth as u32) - 1) as f64;
+    if config.verbose {
+        println!("Max sample value: {}", max);
+    }
+
+    for i in 0..config.samples {
+        let x = i as f64 / config.samples as f64;
+        let sin = (x * 2_f64 * PI).sin();
+        let sample = (((sin + 1_f64) / 2_f64) * max).round() as u32;
+        if config.hex {
+            println!("{:#010x}", sample)
+        } else {
+            println!("{}", sample);
+        }
+    }
+}
